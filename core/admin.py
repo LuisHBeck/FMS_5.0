83bb7f27b078b201303b992a7b781714d20614cd
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-17 11:14:38 -0300

    feat: create models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Machine, Color, Order
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'create', 'active']
+    
+    
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'create', 'active']
+    
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'requester', 'personalization', 'color', 'stage', 'finished']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Base(models.Model):
+    create = models.DateTimeField(auto_now_add=True)
+    modify = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        abstract = True
+        
+
+class Machine(Base):
+    name = models.CharField(max_length=20)
+    
+    class Meta:
+        verbose_name = 'Machine'
+        verbose_name_plural = 'Machines'
+        
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class Color(Base):
+    name = models.CharField(max_length=15)
+    
+    class Meta:
+        verbose_name = 'Color'
+        verbose_name_plural = 'Colors'
+        
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class Order(Base):
+    requester = models.ForeignKey(User, on_delete=models.SET('unknown'))
+    personalization = models.IntegerField()
+    color = models.ForeignKey(Color, on_delete=models.SET('unknown'))
+    STAGE_CHOICE = (('required','Required'), ('production','Production'), ('finished','Finished'))
+    stage = models.CharField(max_length=15, choices=STAGE_CHOICE)
+    finished = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+        
+    def __str__(self) -> str:
+        return f'{self.id} {self.requester}'
+    
+    def save(self, *args, **kwargs):
+        if not self.active and not self.finished:
+            self.finished = timezone.now()
+        super(Order, self).save(*args, **kwargs) 
