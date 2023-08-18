a3740c9980c9c246d34c184ec70de2275d923c06
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-18 09:55:30 -0300

    feat: create demand filter

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 from .models import Order
 from .forms import OrderModelForm
@@ -11,14 +12,15 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class OrderView(FormView):
+class OrderView(UserPassesTestMixin, FormView):
     template_name = 'order.html'
     form_class = OrderModelForm
     success_url = reverse_lazy('order')
+    login_url = reverse_lazy('index')
 
     def form_valid(self, form):
         instance = form.save(commit=False)
-        instance.requester = self.request.user.get_full_name()
+        instance.requester = self.request.user
         instance.save()
         messages.success(self.request, 'Saved successfully')
         return super().form_valid(form)
@@ -27,11 +29,24 @@ class OrderView(FormView):
         messages.error(self.request, 'Error saving')
         return super(OrderView, self).form_valid(form, *args, **kwargs)
     
+    def test_func(self):
+        return self.request.user.is_authenticated
+    
 
-class DemandView(TemplateView):
+class DemandView(UserPassesTestMixin, TemplateView):
     template_name = 'demand.html'
+    login_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(DemandView, self).get_context_data(**kwargs)
-        context['demand'] = Order.objects.all()
+        queryset = Order.objects.all()
+        selected_filter = self.request.GET.get('filter', None)
+        
+        if selected_filter:
+            queryset = queryset.filter(machine=selected_filter)
+        
+        context['demand'] = queryset
         return context
+    
+    def test_func(self):
+        return self.request.user.is_authenticated
